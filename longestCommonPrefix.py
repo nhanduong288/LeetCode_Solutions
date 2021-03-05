05455aad9ad31a72ebca6e00cd4445cfa872ae96
Nhan Duong <duonggiangnhan.vn@gmail.com>
2021-03-04 21:44:38 -0500

    adding comments for longestCommonPrefix

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -1,26 +1,31 @@
 # leetcode easy
 
 def longestCommonPrefix(strs):
-    result = ''
-    if len(strs) < 1:
-        result = ''
-    elif len(strs) == 1:
-        if len(strs[0]) >= 1:
-            result += strs[0][0]
-        else:
-            result == ''
-    else:
-        for i in range(len(strs)-1):
-            if strs[i][:1] == strs[i+1][:1] and len(strs[i]) >= 1 and len(strs[i+1]) >= 1:
-                result += strs[0][:2]
-                break
-            elif strs[i][0] == strs[i+1][0] and len(strs[i]) >= 1 and len(strs[i+1]) >= 1:
-                result += strs[0]
+    # no string = no prefix
+    if len(strs) == 0:
+        return ""
+
+    # compare other strings to the very first one
+    hold = strs[0]
+
+    for i in range(1, len(strs)):
+        prefix = ''
+
+        # if the first string is empty, there is no common prefix
+        if len(hold) == 0:
+            break
+        
+        for j in range(len(strs[i])):
+            
+            if j < len(hold) and hold[j] == strs[i][j]:
+                prefix += hold[j]
             else:
-                result = ''
-    return result
+                break
+        hold = prefix
+    return hold
 
 print(longestCommonPrefix(['dog', 'racecar', 'car']))
 print(longestCommonPrefix(['flower', 'flow', 'flight']))
 print(longestCommonPrefix(['a']))
 print(longestCommonPrefix(['', 'b']))
+print(longestCommonPrefix(['reflower', 'flow', 'flight']))
