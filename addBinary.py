d4f4bdee31a96d3e8a00993d87e4c40ac8d45a3d
Nhan Duong <nhanduong@mail.usf.edu>
2021-03-09 18:57:04 -0500

    working on addBinary

--- addBinary.py
+++ addBinary.py
@@ -2,4 +2,8 @@
 # Given two binary strings a and b, return their sum as a binary string.
 
 def addBinary(a, b):
-    
+    integer_sum = int(a, 2) + int(b, 2)
+    binary_sum = bin(integer_sum)
+    print(binary_sum)
+
+addBinary('11', '1')
