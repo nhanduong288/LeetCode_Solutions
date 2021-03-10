8ac5ec2deaca32d0919312ff1f4792e4cd4d3af3
Nhan Duong <nhanduong@mail.usf.edu>
2021-03-10 11:02:56 -0500

    working on addBinary

--- addBinary.py
+++ addBinary.py
@@ -2,8 +2,30 @@
 # Given two binary strings a and b, return their sum as a binary string.
 
 def addBinary(a, b):
-    integer_sum = int(a, 2) + int(b, 2)
-    binary_sum = bin(integer_sum)
-    print(binary_sum)
+    # Resultant string
+    result = ""
+    # Indices for a and b
+    aCount = len(a) - 1
+    bCount = len(b) - 1
+    # Carry
+    carry = 0
+    # Loop for all the characters in the strings
+    while aCount >= 0 or bCount >= 0:
+        # Sum of two bits
+        totalSum = carry
+        if aCount >= 0:
+            totalSum += int(a[aCount])
+            aCount -= 1
+        if bCount >= 0:
+            totalSum += int(b[bCount])
+            bCount -= 1
+        # Add the bit to te result
+        result = str(totalSum % 2) + result
+        # Modify carry
+        carry = totalSum // 2
+    # Final check if carry exists
+    if carry > 0:
+        result = str(1) + result
+    return result
 
-addBinary('11', '1')
+print(addBinary('11', '1'))
